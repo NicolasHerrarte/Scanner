2006c279da09a787602bc4d6d1b1c0a333672bc2
NicolasHerrarte <nicolasherrarte470@gmail.com>
2025-12-17 08:41:11 -0600

    breaking 10 generations of if else

--- scanner.c
+++ scanner.c
@@ -37,88 +37,101 @@ typedef struct Fragment{
     int end_index;
 } Fragment;
 
+bool altercation(Fragment *left_fragment, Fragment *right_fragment, int *priority, int depth, int i){
+    if(ALT_PRIORITY < *priority && depth == 0){
+        left_fragment->end_index = i;
+        right_fragment->start_index = i+1;
+        *priority = ALT_PRIORITY;
+        return true;
+    }
+    return false;
+}
 
-int find_split_point(char *str, int start_index, int end_index, bool recursion){
-    int left_index = start_index;
-    int right_index = end_index-1;
+
+int find_split_point(char *str, Fragment fragment, bool recursion){
+    Fragment left_fragment = {fragment.start_index, fragment.start_index};
+    Fragment right_fragment = {fragment.end_index, fragment.end_index};
+
     int min_priority = 3;
     int parenthesis_depth = 0;
 
     bool final_split = false;
     bool split_found = false;
+    bool found_solution = false;
 
-    for(int i = start_index;i<end_index;i++){
+    for(int i = fragment.start_index;i<fragment.end_index;i++){
         printf("%c", str[i]);
     }
     printf("\n");
 
-    if(start_index == end_index-1){
+    if(fragment.start_index == fragment.end_index-1){
         return 0;
     }
 
     else{
-        for(int i = start_index;i<end_index;i++){
-        char current_char = str[i];
-        if(current_char == '|'){
-            if(ALT_PRIORITY < min_priority && parenthesis_depth == 0){
-                left_index = i;
-                right_index = i+1;
-                min_priority = ALT_PRIORITY;
+        for(int i = fragment.start_index;i<fragment.end_index;i++){
+            char current_char = str[i];
+            if(current_char == '|'){
+                found_solution = altercation(&left_fragment, &right_fragment, &min_priority, parenthesis_depth, i);
+            }
+            else if(current_char == '*'){
+                if(CLOS_PRIORITY < min_priority && parenthesis_depth == 0){
+                    if(i == fragment.end_index-1 && split_found == false){
+                        final_split = true;
+                        left_fragment.end_index = i;
+                        min_priority = CLOS_PRIORITY;
+                        found_solution = true;
+                    }
+                    else{
+                        left_fragment.end_index = i;
+                        right_fragment.start_index = i+1;
+                        min_priority = CLOS_PRIORITY;
+                        found_solution = true;
+                    }
+                }
+            }
+            else if(current_char == '('){
+                if(CONCAT_PRIORITY < min_priority && parenthesis_depth == 0){
+                    if(i >= fragment.start_index+1){
+                        left_fragment.end_index = i;
+                        right_fragment.start_index = i;
+                        min_priority = CONCAT_PRIORITY;
+                        found_solution = true;
+                    }
+                }
+                parenthesis_depth += 1;
+            }
+            else if(current_char == ')'){
+                parenthesis_depth -= 1;
+                if(i == fragment.end_index-1 && split_found == false){
+                    final_split = true;
+                    left_fragment.start_index += 1;
+                    left_fragment.end_index = i;
+                    found_solution = true;
+                }
+            }
+            else{
+                if(CONCAT_PRIORITY < min_priority && parenthesis_depth == 0){
+                    if(i >= fragment.start_index+1){
+                        left_fragment.end_index = i;
+                        right_fragment.start_index = i;
+                        min_priority = CONCAT_PRIORITY;
+                        found_solution = true;
+                    }
+                    else{
+                        if(str[i+1] != '*'){
+                            left_fragment.end_index = i+1;
+                            right_fragment.start_index = i+1;
+                            min_priority = CONCAT_PRIORITY;
+                            found_solution = true;
+                        } 
+                    }
+                }
+            }
+
+            if(found_solution == true){
                 split_found = true;
             }
-        }
-        else if(current_char == '*'){
-            if(CLOS_PRIORITY < min_priority && parenthesis_depth == 0){
-                if(i == end_index-1 && split_found == false){
-                    final_split = true;
-                    left_index = i;
-                    min_priority = CLOS_PRIORITY;
-                }
-                else{
-                    left_index = i;
-                    right_index = i+1;
-                    min_priority = CLOS_PRIORITY;
-                    split_found = true;
-                }
-            }
-        }
-        else if(current_char == '('){
-            if(CONCAT_PRIORITY < min_priority && parenthesis_depth == 0){
-                if(i >= start_index+1){
-                    left_index = i;
-                    right_index = i;
-                    min_priority = CONCAT_PRIORITY;
-                    split_found = true;
-                }
-            }
-            parenthesis_depth += 1;
-        }
-        else if(current_char == ')'){
-            parenthesis_depth -= 1;
-            if(i == end_index-1 && split_found == false){
-                final_split = true;
-                start_index += 1;
-                left_index = i;
-            }
-        }
-        else{
-            if(CONCAT_PRIORITY < min_priority && parenthesis_depth == 0){
-                if(i >= start_index+1){
-                    left_index = i;
-                    right_index = i;
-                    min_priority = CONCAT_PRIORITY;
-                    split_found = true;
-                }
-                else{
-                    if(str[i+1] != '*'){
-                        left_index = i+1;
-                        right_index = i+1;
-                        min_priority = CONCAT_PRIORITY;
-                        split_found = true;
-                    } 
-                }
-            }
-        }
     }
     }
 
@@ -130,11 +143,11 @@ int find_split_point(char *str, int start_index, int end_index, bool recursion){
 
     if(recursion == true){
         if(final_split == true){
-            find_split_point(str, start_index, left_index, true);
+            find_split_point(str, left_fragment, true);
         }
         else{
-            find_split_point(str, start_index, left_index, true);
-            find_split_point(str, right_index, end_index, true);
+            find_split_point(str, left_fragment, true);
+            find_split_point(str, right_fragment, true);
         }
     }
 }
@@ -143,6 +156,7 @@ int main() {
     printf("Scanner...\n");
     char regex[] = "((((ab|abc|ababc)(bc|c|cde)*)|((ab|abc)*(bc|cde|de)))*(((abc|ab)(bc|cde)*)|((ab|abc)*(bc|c)))*((ab|abc|ababc)*(bc|c)*)*)*((((ab|abc)c*(de|d)|(abc|bc)*(ab|c))((ab|bc)d*|abc))*((ab|abc)d|c*))*(((abc|ab)(bc|cde)*)|(ab|bc)*(cd|de))*((ab|abc)*(bc|c))*)";
     //printf("%d\n", strlen(regex));
-    find_split_point(regex, 0, strlen(regex), true);
+    Fragment fragment_start = {0, strlen(regex)};
+    find_split_point(regex, fragment_start, true);
     return 0;
 }
