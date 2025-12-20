b00ba90dc0b2ca653d68556e7c349fcecfec3bad
NicolasHerrarte <nicolasherrarte470@gmail.com>
2025-12-20 13:10:32 -0600

    some safety measures and dynarray

--- scanner.c
+++ scanner.c
@@ -3,13 +3,20 @@
 #include <stdbool.h>
 #include <string.h>
 #include <assert.h>
-#include "structs.h"
+#include "dynarray.h"
 
 #define MAX_REGEX_LENGTH 32
 
 #define ALT_PRIORITY 0
 #define CONCAT_PRIORITY 1
 #define CLOS_PRIORITY 2
+#define FIN_PRIORITY 3
+
+typedef struct Transition{
+    int state_from;
+    int state_to;
+    char trans_char;
+} Transition;
 
 typedef struct NFA{
     int* states;
@@ -20,20 +27,31 @@ typedef struct NFA{
     int head;
     int tail;
 
-    int state_amount;
-    int transition_amount;
-    int acceptable_state_amount;
-
 } NFA;
 
+int NFA_initialize(NFA *nfa){
+    nfa->states = dynarray_create(int);
+    nfa->alphabet = dynarray_create(char);
+    nfa->transitions = dynarray_create(Transition);
+    nfa->acceptable_states = dynarray_create(int);
+}
+
 int NFA_next_state(NFA *nfa){
-    if(nfa->state_amount == 0){
-        nfa->states = malloc(1*sizeof(int));
-        (nfa->states)[0] = 0;
-    }
-    else{
-        nfa->states = realloc(nfa->states, 1*sizeof(int));
-    }
+    int next_int = dynarray_length(nfa->states);
+    dynarray_push(nfa->states, next_int);
+
+    return next_int;
+}
+
+Transition NFA_add_transition(NFA *nfa, int _from, int _to, char _trans_char){
+    Transition trans;
+    trans.state_from = _from;
+    trans.state_to = _to;
+    trans.trans_char = _trans_char;
+    dynarray_push(nfa->transitions, trans);
+    dynarray_push(nfa->alphabet, _trans_char);
+
+    return trans;
 }
 
 typedef struct Fragment{
@@ -41,7 +59,7 @@ typedef struct Fragment{
     int end_index;
 } Fragment;
 
-bool altercation(Fragment *left_fragment, Fragment *right_fragment, int *priority, int depth, int i){
+int altercation(Fragment *left_fragment, Fragment *right_fragment, int *priority, int depth, int i){
     if(ALT_PRIORITY < *priority && depth == 0){
         left_fragment->end_index = i;
         right_fragment->start_index = i+1;
@@ -51,7 +69,7 @@ bool altercation(Fragment *left_fragment, Fragment *right_fragment, int *priorit
     return false;
 }
 
-bool concatenation(Fragment fragment, Fragment *left_fragment, Fragment *right_fragment, int *priority, int depth, int i, char next_char){
+int concatenation(Fragment fragment, Fragment *left_fragment, Fragment *right_fragment, int *priority, int depth, int i, char next_char){
     if(CONCAT_PRIORITY < *priority && depth == 0){
         if(i >= fragment.start_index+1){
             left_fragment->end_index = i;
@@ -60,7 +78,7 @@ bool concatenation(Fragment fragment, Fragment *left_fragment, Fragment *right_f
             return true;
         }
         else{
-            if(next_char != '\0' && next_char != '*'){
+            if(next_char != '\0' && next_char != '*' && next_char != '$'){
                 left_fragment->end_index = i+1;
                 right_fragment->start_index = i+1;
                 *priority = CONCAT_PRIORITY;
@@ -71,25 +89,26 @@ bool concatenation(Fragment fragment, Fragment *left_fragment, Fragment *right_f
     return false;
 }
 
-bool closure(Fragment fragment, Fragment *left_fragment, Fragment *right_fragment, int *priority, bool *final_split, int depth, bool split_found, int i){
-    if(CLOS_PRIORITY < *priority && depth == 0){
-        if(i == fragment.end_index-1 && split_found == false){
+int closure(Fragment fragment, Fragment *left_fragment, Fragment *right_fragment, int *priority, bool *final_split, int depth, bool split_found, int pass_priority, int i){
+    if(pass_priority <= *priority && depth == 0){
+        if(i == fragment.end_index-1){
             left_fragment->end_index = i;
             *final_split = true;
-            *priority = CLOS_PRIORITY;
+            *priority = pass_priority;
             return true;
         }
         else{
-            left_fragment->end_index = i;
-            right_fragment->start_index = i+1;
-            *priority = CLOS_PRIORITY;
-            return true;
+            //printf("EARLY\n");
+            //left_fragment->end_index = i;
+            //right_fragment->start_index = i+1;
+            //*priority = pass_priority;
+            //return true;
         }
     }
     return false;
 }
 
-bool parenthesis(Fragment fragment, Fragment *left_fragment, bool *final_split, bool split_found, int i){
+int parenthesis(Fragment fragment, Fragment *left_fragment, bool *final_split, bool split_found, int i){
     if(i == fragment.end_index-1 && split_found == false){
         left_fragment->start_index += 1;
         left_fragment->end_index = i;
@@ -100,11 +119,11 @@ bool parenthesis(Fragment fragment, Fragment *left_fragment, bool *final_split, 
 }
 
 
-int find_split_point(char *str, Fragment fragment, bool recursion){
+int find_split_point(NFA* nfa, char* str, Fragment fragment, bool recursion){
     Fragment left_fragment = {fragment.start_index, fragment.start_index};
     Fragment right_fragment = {fragment.end_index, fragment.end_index};
 
-    int min_priority = 3;
+    int min_priority = 4;
     int parenthesis_depth = 0;
 
     bool final_split = false;
@@ -117,6 +136,9 @@ int find_split_point(char *str, Fragment fragment, bool recursion){
     printf("\n");
 
     if(fragment.start_index == fragment.end_index-1){
+        int state_head = NFA_next_state(nfa);
+        int state_tail = NFA_next_state(nfa);
+        Transition trans = NFA_add_transition(nfa, state_head, state_tail, str[fragment.start_index]);
         return 0;
     }
 
@@ -127,7 +149,10 @@ int find_split_point(char *str, Fragment fragment, bool recursion){
                 found_solution = altercation(&left_fragment, &right_fragment, &min_priority, parenthesis_depth, i);
             }
             else if(current_char == '*'){
-                found_solution = closure(fragment, &left_fragment, &right_fragment, &min_priority, &final_split, parenthesis_depth, split_found, i);
+                found_solution = closure(fragment, &left_fragment, &right_fragment, &min_priority, &final_split, parenthesis_depth, split_found, CLOS_PRIORITY, i);
+            }
+            else if(current_char == '$'){
+                found_solution = closure(fragment, &left_fragment, &right_fragment, &min_priority, &final_split, parenthesis_depth, split_found, FIN_PRIORITY, i);
             }
             else if(current_char == '('){
                 found_solution = concatenation(fragment, &left_fragment, &right_fragment, &min_priority, parenthesis_depth, i, '\0');
@@ -141,7 +166,7 @@ int find_split_point(char *str, Fragment fragment, bool recursion){
                 found_solution = concatenation(fragment, &left_fragment, &right_fragment, &min_priority, parenthesis_depth, i, str[i+1]);
             }
 
-            if(found_solution == true){
+            if(found_solution > 0){
                 split_found = true;
             }
     }
@@ -152,28 +177,44 @@ int find_split_point(char *str, Fragment fragment, bool recursion){
         return 0;
     }
 
+    //printf("(%d, %d) (%d, %d)\n",left_fragment.start_index, left_fragment.end_index, right_fragment.start_index, left_fragment.end_index);
+
     if(recursion == true){
         if(final_split == true){
-            find_split_point(str, left_fragment, true);
+            find_split_point(nfa, str, left_fragment, true);
         }
         else{
-            find_split_point(str, left_fragment, true);
-            find_split_point(str, right_fragment, true);
+            switch(min_priority){
+                case ALT_PRIORITY:
+                    printf("ALTERCATION\n");
+                    break;
+                case CONCAT_PRIORITY:
+                    printf("CONCATENATION\n");
+                    break;
+                default:
+                    printf("Something is not right\n");
+            }
+            find_split_point(nfa, str, left_fragment, true);
+            find_split_point(nfa, str, right_fragment, true);
         }
     }
 }
+// final test ->  "(((ab|abc|a)*(bc|c|cde)*)*((abc|ab|(a|b|c)*)*(de|d|(ab|abc)*)*)|((ab|abc|ababc)*((bc|c|cde)*|(a|b|c)*))*)*"
 
 int main() {
     printf("Scanner...\n");
+
+    NFA nfa;
+    NFA_initialize(&nfa);
     char regex[] = "(((ab|abc|a)*(bc|c|cde)*)*((abc|ab|(a|b|c)*)*(de|d|(ab|abc)*)*)|((ab|abc|ababc)*((bc|c|cde)*|(a|b|c)*))*)*";
     Fragment fragment_start = {0, strlen(regex)};
-    find_split_point(regex, fragment_start, true);
+    find_split_point(&nfa, regex, fragment_start, true);
+
+
+    //Fragment fragment_start1 = {0, 4};
+    //find_split_point(&nfa, regex, fragment_start1, false);
+    //Fragment fragment_start2 = {5, 4};
+    //find_split_point(&nfa, regex, fragment_start2, false);
 
     printf("Structs...\n");
-
-    Array arr;
-    int t[5]  = {1,2,3,4,5};
-    initialize_int(&arr, t, 5);
-    print_int("%d", arr);
-    return 0;
 }
